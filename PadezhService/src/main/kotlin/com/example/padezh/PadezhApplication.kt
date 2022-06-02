d366b44d87fac68297ebb24d16c41ecce8d48144
Kirill Yakovlev <k.yakovlev@bftcom.com>
2022-06-02 14:44:24 +0300

    Замечания

--- PadezhService/src/main/kotlin/com/example/padezh/PadezhApplication.kt
+++ PadezhService/src/main/kotlin/com/example/padezh/PadezhApplication.kt
@@ -13,10 +13,10 @@ fun main() {
 
     runApplication<PadezhApplication>()
 }
-
+// TODO вынести логику формирования всех форм слова в отдлеьный @Service класс и внедрять его в контроллер через @Autowired
 @RestController
 class ChangingPedezh{
-
+    // TODO https://google.github.io/styleguide/javaguide.html 4.8.5.2 Class annotations форматирование кода аннотаций
     @RequestMapping(value =["/spell/{word}"], method = [(RequestMethod.GET)])
 
     @ResponseBody
@@ -24,7 +24,7 @@ class ChangingPedezh{
 
         // https://morpher.ru/My/Token.aspx - для генерации токена
         val russianDeclensionResult: DeclensionResult = ClientBuilder()
-            .useToken("a7baa109-f9db-4911-bf09-5d37cf015e12")
+            .useToken("a7baa109-f9db-4911-bf09-5d37cf015e12") // TODO токен выносится в application.properties, чтобы другие пользователи могли использовать свой, иначе придется всегда менять исходный код и перекомиплировать проект
             .build()
             .russian()
             .declension(word)
--- PadezhService/src/test/kotlin/com/example/padezh/ChangingPedezhTest.kt
+++ PadezhService/src/test/kotlin/com/example/padezh/ChangingPedezhTest.kt
@@ -3,12 +3,14 @@ package com.example.padezh
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+// TODO переделать на спринговый тест с внедрением сервисного класса для склонения
 internal class ChangingPadezhTests{
     private val result: ChangingPedezh = ChangingPedezh()
 
     private val word : String = "Елка"
 
-
+    // TODO тест плохой, не проверяются формы падежей
+    // нужно что-то типа assertEquals("Елка", result.get({индекс именительного падежа}))
     @Test
     fun spell_numberOfCases_Equals (){
 
